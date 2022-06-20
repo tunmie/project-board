1ee877c03d6c7809cb44a0af9468078f2833f2c2
tunmie <victoroluwasomidotun@gmail.com>
2022-06-20 06:57:12 +0100

    project code synced with book

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,47 @@
+import Lane from '../../components/Lane/Lane';
+import './Board.css';
+import {useState, useEffect} from "react";
+
+const lanes = [
+	{ id: 1, title: 'To Do' },
+	{ id: 2, title: 'In Progress' },
+	{ id: 3, title: 'Review' },
+	{ id: 4, title: 'Done' },
+];
+
+function Board() {
+	const [loading, setLoading] = useState(false);
+	const [tasks, setTasks] = useState([]);
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const tasks = await fetch(
+					`https://my-json-server.typicode.com/
+					PacktPublishing/React-Projects-Second-Edition/tasks`,
+				);
+				const result = await tasks.json();
+				if (result) {
+					setTasks(result);
+					setLoading(false);
+				}
+			}catch (e) {
+				setLoading(false);
+				setError(e.message);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className='Board-wrapper'>
+			{lanes.map((lane) => (
+				<Lane key={lane.id} title={lane.title} />
+			))}
+		</div>
+	);
+}
+
+export default Board;
